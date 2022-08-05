06d3239187108cf3b4a5e6a743b1754fe5dcbf2f
Omolara5861 <debbiegterra@gmail.com>
2022-08-05 10:30:41 +0100

    added routing to training

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TrainingComponent } from './training/training.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'training.zuri.team', component: TrainingComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
